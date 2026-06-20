91ccc2934643275bb8cc11405da4539a53bde417
Vedika2554 <vedikayadav652@gmail.com>
2026-06-20 13:20:30 +0530

    product page work start

---
+++ js/product-data.js
@@ -0,0 +1,16 @@
+const products = [
+    {
+        id: 1,
+        name: "HP 15 Smartchoice, 13th Gen Intel Core i3-1315U (12GB DDR4, 512GB SSD) FHD, Anti-Glare, Micro-Edge, 15.6''/39.6cm, Win11, M365(1yr)*Office24, Silver, 1.59kg, fd0573TU, FHD Camera w/Shutter Laptop",
+        originalPrice: 69990,
+        price: 55990,
+        category: "electronics",
+        images: [
+            "https://m.media-amazon.com/images/I/61ER6I1s9iL._SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/714S3yy4NKL._SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/71fDAMEQLRL._SL1500_.jpg",
+            "https://m.media-amazon.com/images/I/61eA3CEPpqL._SL1500_.jpg"
+        ]
+    }
+];
+
---
+++ js/product.js
@@ -0,0 +1,158 @@
+const container =
+document.getElementById("productsContainer");
+
+const search =
+document.getElementById("search");
+
+
+function displayProducts(data){
+
+    container.innerHTML = "";
+
+    data.forEach(product => {
+
+        const discount = Math.round(
+            ((product.originalPrice - product.price) /
+            product.originalPrice) * 100
+        );
+
+        container.innerHTML += `
+        
+        <div class="card">
+
+            <img src="${product.images[0]}">
+
+            <h3>${product.name}</h3>
+
+            <div class="price-box">
+            
+                <del>
+                    ₹${product.originalPrice.toLocaleString()}
+                </del>
+
+                <span class="new-price">
+                    ₹${product.price.toLocaleString()}
+                </span>
+
+                <span class="discount">
+                    ${discount}% OFF
+                </span>
+
+            </div>
+
+         <div class="btn-box">
+
+                <button
+                onclick="viewDetails(${product.id})">
+                    View Details
+                </button>
+
+                <button
+                onclick="addToCart(${product.id})">
+                    Add To Cart
+                </button>
+
+            </div>
+
+        </div>
+
+        `;
+    });
+
+}
+
+  
+/* Category Filter */
+
+function showProducts(category){
+
+    let filteredProducts =
+    products.filter(product =>
+    product.category === category
+    );
+
+    displayProducts(filteredProducts);
+
+}
+
+/* Search Filter */
+
+search.addEventListener(
+"keyup",
+function(){
+
+    let keyword =
+    this.value.toLowerCase();
+
+    let result =
+    products.filter(product =>
+
+    product.name
+    .toLowerCase()
+    .includes(keyword)
+
+    );
+
+    displayProducts(result);
+
+});
+function viewDetails(id){
+
+    localStorage.setItem(
+    "selectedProduct",
+    id
+    );
+
+    window.location.href =
+    "product-detail.html";
+
+}
+function addToCart(id){
+
+    let currentUser =
+    JSON.parse(
+    localStorage.getItem("currentUser")
+    );
+
+    if(!currentUser){
+
+        alert("Please Login First");
+
+        window.location.href =
+        "register.html";
+
+        return;
+    }
+
+    let cartKey =
+    "cart_" + currentUser.mobile;
+
+    let cart =
+    JSON.parse(
+    localStorage.getItem(cartKey)
+    ) || [];
+
+    let product =
+    products.find(
+    p => p.id === id
+    );
+
+    cart.push(product);
+
+    localStorage.setItem(
+    cartKey,
+    JSON.stringify(cart)
+    );
+
+    alert(
+    product.name +
+    " Added To Cart"
+    );
+
+    window.location.href =
+    "cart.html";
+
+}
+
+
+showProducts("electronics");
